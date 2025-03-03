8a3584ec1a32054f3d4ff5252053abe81740c88d
Khumeren <khucode@gmail.com>
2025-03-03 11:22:34 +0800

    refactor: added displayName to payment method

--- FinanceTracker.Domain/Enums/PaymentMethod.cs
+++ FinanceTracker.Domain/Enums/PaymentMethod.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace FinanceTracker.Domain.Enums
 {
     public enum PaymentMethod
     {
+        [Display(Name = "Cash")]
         Cash = 1,
+
+        [Display(Name = "Credit Card")]
         CreditCard = 2,
+
+        [Display(Name = "QR Code Payment")]
         QRCodePayment = 3,
+
+        [Display(Name = "eWallet")]
         EWallet = 4,
+
+        [Display(Name = "Bank Transfer")]
         BankTransfer = 5,
+
+        [Display(Name = "DuitNow")]
         DuitNow = 6
     }
 }
