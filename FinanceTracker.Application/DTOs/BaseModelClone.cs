62e121b1f6c5487db26748943e5acac10b518e1c
Khumeren <khucode@gmail.com>
2025-03-09 13:04:29 +0800

    feat: base dto entity for API responses

---
+++ FinanceTracker.Application/DTOs/BaseModelClone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Application.DTOs
+{
+    internal class BaseModelClone
+    {
+    }
+}
