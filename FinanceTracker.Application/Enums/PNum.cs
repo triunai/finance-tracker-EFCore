298c830284f6316bbf5dad09aeb3a31e8e617e75
Khumeren <khucode@gmail.com>
2025-03-23 15:40:33 +0800

    refactor: aligned payment methods logic with DB

---
+++ FinanceTracker.Application/Enums/PNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Domain.Enums
+{
+    public enum PNum
+    {
+        [Display(Name = "Cash")]
+        Cash = 1,
+
+        [Display(Name = "Credit Card")]
+        CreditCard = 2,
+
+        [Display(Name = "QR Code Payment")]
+        QRCodePayment = 3,
+
+        [Display(Name = "eWallet")]
+        EWallet = 4,
+
+        [Display(Name = "Bank Transfer")]
+        BankTransfer = 5,
+
+        [Display(Name = "DuitNow")]
+        DuitNow = 6
+    }
+}
--- FinanceTracker.Domain/Models/Category.cs
+++ FinanceTracker.Domain/Models/Category.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinanceTracker.Domain.Models
 {
+    [Table("budget_category")]
     public class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int? ParentCategoryId { get; set; }
-        public virtual Category ParentCategory { get; set; }
-        public virtual ICollection<Category> SubCategories { get; set; }
-        public virtual ICollection<ExpenseItem> ExpenseItems { get; set; }
+        [Key, Column(Order = 0)]
+        public long BudgetId { get; set; }
+        [Key, Column(Order = 1)]
+        public long CategoryId { get; set; }
+
+        public decimal? AlertThreshold { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual Budget Budget { get; set; }
+        public virtual ExpenseCategory ExpenseCategory { get; set; }
     }
 }
